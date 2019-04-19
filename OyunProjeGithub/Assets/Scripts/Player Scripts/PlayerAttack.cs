2b9c779d94b477f7c3454ed8844f7955f489588c
gokalpyanik <48530698+gokalpyanik@users.noreply.github.com>
2019-04-19 15:44:45 +0300

    fps camera ve silahlar

---
+++ OyunProjeGithub/Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    private WeaponManager weapon_Manager;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    private bool is_Aiming;
+
+    [SerializeField]
+    private GameObject arrow_Prefab, spear_Prefab;
+
+    [SerializeField]
+    private Transform arrow_Bow_StartPosition;
+
+    void Awake()
+    {
+
+        weapon_Manager = GetComponent<WeaponManager>();
+
+        
+        mainCam = Camera.main;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WeaponShoot();
+//ZoomInAndOut();
+    }
+
+    void WeaponShoot()
+    {
+
+        // if we have assault riffle
+       
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                // handle axe
+                if (weapon_Manager.GetCurrentSelectedWeapon().tag == "Axe"&& Input.GetMouseButtonDown(0))
+                {
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+
+                // handle shoot
+                if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+
+                    weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                   // BulletFired();
+
+                }
+           
+
+
+            } // if input get mouse button 0
+
+        } // else
+
+   // weapon shoot
+
+  
+    void BulletFired()
+    {
+        print("zasdca");
+        RaycastHit hit;
+
+        if (Physics.Raycast(mainCam.transform.position, mainCam.transform.forward, out hit))
+        {
+
+            print("WE HIT: " + hit.transform.gameObject.name);
+
+        }
+
+    } // bullet fired
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ OyunProjeGithub/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController mahmut_Controller;
+
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump_Force = 10f;
+    private float vertical_Velocity;
+
+    void Awake()
+    {
+        mahmut_Controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MoveMahmut();
+    }
+
+    void MoveMahmut()
+    {
+
+        move_Direction = new Vector3(Input.GetAxis("Horizontal"), 0f,
+                                     Input.GetAxis("Vertical"));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        mahmut_Controller.Move(move_Direction);
+
+
+    } // move player
+
+    void ApplyGravity()
+    {
+
+        vertical_Velocity -= gravity * Time.deltaTime;
+
+        // jump
+        PlayerJump();
+
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    } // apply gravity
+
+    void PlayerJump()
+    {
+
+        if (mahmut_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_Velocity = jump_Force;
+        }
+
+    }
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
